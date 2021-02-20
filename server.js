3590be81c076467bec68a87fab9bb8ef0604999a
Yusuf Eryilmaz <yusuferyilmaz1819@gmail.com>
2021-02-20 11:12:22 -0500

    wp-backend

---
+++ server.js
@@ -0,0 +1,87 @@
+//importing
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import Messages from "./dbMessages.js";
+import Pusher from "pusher";
+import cors from "cors";
+
+//app config
+const app = express();
+dotenv.config();
+const port = process.env.PORT || 8000;
+
+const pusher = new Pusher({
+  appId: process.env.APP_ID,
+  key: process.env.KEY,
+  secret: process.env.SECRET,
+  cluster: "us2",
+  useTLS: true,
+});
+
+//middleware
+app.use(express.json());
+app.use(cors());
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Headers", "*");
+//   next();
+// });
+
+//DB Config
+const connection_url = process.env.CONNECTION_URL;
+mongoose.connect(connection_url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+//??????
+const db = mongoose.connection;
+db.once("open", () => {
+  console.log("DB connected");
+  const msgCollection = db.collection("messagecontents");
+  const changeStream = msgCollection.watch();
+  changeStream.on("change", (change) => {
+    console.log(change);
+    if (change.operationType == "insert") {
+      const messageDetails = change.fullDocument;
+      pusher.trigger("messages", "inserted", {
+        name: messageDetails.name,
+        message: messageDetails.message,
+        timestamp: messageDetails.timestamp,
+        received: messageDetails.received,
+      });
+    } else {
+      console.log("Error triggering pusher");
+    }
+  });
+});
+
+//api routes
+app.get("/", (req, res) => res.status(200).send("hello yusuf"));
+
+app.get("/messages/sync", (req, res) => {
+  Messages.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+});
+
+app.post("/messages/new", (req, res) => {
+  const dbMessage = req.body;
+
+  Messages.create(dbMessage, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+});
+
+//listen
+app.listen(port, () => console.log(`listening port: ${port}`));
